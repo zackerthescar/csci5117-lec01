99f64491f2b5621ab687e0a8db56d77aa3691138
Autumn <auctumnus@pm.me>
2024-09-09 16:54:15 -0500

    fix: give up and wait for DOMContentLoaded

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const guestbookList = document.getElementById('guestbook-list');
 // should be a <button>
 const guestbookButton = document.getElementById('guestbook-button');
 
-guestbookButton.addEventListener('click', () => {
-    const name = prompt('What is your name?');
-    const li = document.createElement('li');
-    li.textContent = name;
-    guestbookList.appendChild(li);
+document.addEventListener('DOMContentLoaded', () => {
+
+    guestbookButton.addEventListener('click', () => {
+        const name = prompt('What is your name?');
+        const li = document.createElement('li');
+        li.textContent = name;
+        guestbookList.appendChild(li);
+    })
 })
