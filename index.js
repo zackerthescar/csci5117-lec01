c00269b7936e1496a328ce30f908c62ad0a68efa
Autumn <auctumnus@pm.me>
2024-09-09 16:51:18 -0500

    add guestbook and basic css

---
+++ index.js
@@ -0,0 +1,12 @@
+// should be a <ul>
+const guestbookList = document.getElementById('guestbook-list');
+
+// should be a <button>
+const guestbookButton = document.getElementById('guestbook-button');
+
+guestbookButton.addEventListener('click', () => {
+    const name = prompt('What is your name?');
+    const li = document.createElement('li');
+    li.textContent = name;
+    guestbookList.appendChild(li);
+})
